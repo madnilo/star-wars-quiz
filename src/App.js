98198c7b81b06ca43aae8d6bbf7e71f8c73ae2cb
Danilo Moura <danilomourag@gmail.com>
2018-08-15 10:34:56 -0300

    Add smoke tests with jest / Still failing to configure enzyme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Main from './components/Main/Main';
 import Start from './components/Start/Start';
@@ -9,8 +9,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={Start} />
-        <Route path='/quiz' component={Main} />
+        <BrowserRouter>
+          <div>
+            <Route exact path='/' component={Start} />
+            <Route path='/quiz' component={Main} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
